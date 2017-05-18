bf6aa7bf9e98a100349ff93056d4c567958a59a2
Yaroslav Protsenko <yprotsenko@4tifier.com>
2017-05-18 21:20:00 +0300

    task 1/1 done

--- practice/1/task1.js
+++ practice/1/task1.js
@@ -1,6 +1,5 @@
 function canBeTriangle(a, b, c) {
-	//Change this function
-	return false;
+	return (a+b > c) && (b+c > a) && (a+c > b);
 }
 
 function assert(expression, message) {
@@ -17,7 +16,7 @@ function runTask1() {
 	assert(canBeTriangle(2.3, 2.3, 2.3)==true, "Equilateral triangle case");
 	assert(canBeTriangle(11.4, 0.8, 11)==true, "Third case");
 	assert(canBeTriangle(15, 30, 15)==false, "Fourth case");
-	assert(canBeTriangle(0, 4, 4)==false, "Zero side case`");
+	assert(canBeTriangle(0, 4, 4)==false, "Zero side case");
 	console.log("Done!");
 }
 
